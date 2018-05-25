90457306adae2e6b173aaf6b8ddc891962375d32
Nguyen Xuan Hao <nxhao235@gmail.com>
2018-05-25 17:25:41 +0700

    test csv

---
+++ app/src/main/java/com/hht/myfi/CSVExporter/CSVExporterActivity.java
@@ -0,0 +1,13 @@
+package com.hht.myfi.CSVExporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CSVExporter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_csvexporter);
+    }
+}
